d06ebafdc6b1af827b9a6acf0ec3cbc4773e56d1
Scott Betzel <scott.betzel@gmail.com>
2016-09-12 21:32:45 -0400

    DarkTemplarKnight tester complete.  About to start in on AI

--- src/js/GameObjects/DarkTemplarKnight.js
+++ src/js/GameObjects/DarkTemplarKnight.js
@@ -7,7 +7,8 @@ define(["THREE",
         "util/Console",
         "util/PathHelper",
         "Animation",
-        "AnimationHandler"],
+        "AnimationHandler",
+        "jquery"],
     function (THREE,
               GameObjectBase,
               ArrayUtils,
@@ -15,15 +16,22 @@ define(["THREE",
               console,
               PathHelper,
               Animation,
-              AnimationHandler) {
+              AnimationHandler,
+              $) {
 
         var DarkTemplarKnight = function () {
             GameObjectBase.call(this);
 
             this.actions = {};
             this.mixer = null;
+            this.state = null;
 
         };
+
+        DarkTemplarKnight.MODE_ATTACK = 0;
+        DarkTemplarKnight.MODE_PATROL = 1;
+        DarkTemplarKnight.MODE_HURT = 2;
+        DarkTemplarKnight.MODE_DEAD = 3;
 
         DarkTemplarKnight.prototype = Object.assign(Object.create(GameObjectBase.prototype), {
 
@@ -38,7 +46,7 @@ define(["THREE",
 
                 var loader = new THREE.JSONLoader();
                 var dataPath = PathHelper.absolutePath("assets/dark_templar_knight/dark_templar_knight.json");
-                // var texturePath = PathHelper.absolutePath("assets/dark_templar_knight/");
+
                 loader.load( dataPath, function (geometry, materials) {
 
                     var material = new THREE.MultiMaterial( materials );
@@ -48,24 +56,9 @@ define(["THREE",
                     object.scale.set(0.15, 0.15, 0.15);
                     object3D.add(object);
 
-                    // var materials = object.material.materials;
-
                     for (var k in material.materials) {
                         materials[k].skinning = true;
                     }
-
-                    // self.actions.attack = new THREE.AnimationAction( geometry.animations[ 0 ] );
-                    // self.actions.attack.weight = 0;
-                    // self.actions.die = new THREE.AnimationAction( geometry.animations[ 1 ] );
-                    // self.actions.die.weight = 0;
-                    // self.actions.hurt = new THREE.AnimationAction( geometry.animations[ 2 ] );
-                    // self.actions.hurt.weight = 0;
-                    // self.actions.idle = new THREE.AnimationAction( geometry.animations[ 3 ] );
-                    // self.actions.idle.weight = 1;
-                    // self.actions.run  = new THREE.AnimationAction( geometry.animations[ 4 ] );
-                    // self.actions.run.weight = 0;
-                    // self.actions.walk = new THREE.AnimationAction( geometry.animations[ 5 ] );
-                    // self.actions.walk.weight = 0;
 
                     self.mixer = new THREE.AnimationMixer(object);
                     self.actions.attack = self.mixer.clipAction(geometry.animations[ 0 ]);  // ATTACK
@@ -75,19 +68,19 @@ define(["THREE",
                     self.actions.run = self.mixer.clipAction(geometry.animations[ 4 ]);  // RUN
                     self.actions.walk = self.mixer.clipAction(geometry.animations[ 5 ]);  // WALK
 
-                    self.actions.attack.setEffectiveWeight( 1 ).play();
-                    self.actions.idle.setEffectiveWeight( 1 ).stop();
+                    self.actions.attack.setEffectiveWeight( 1 ).stop();
+                    self.actions.idle.setEffectiveWeight( 1 ).play();
                     self.actions.die.setEffectiveWeight( 1 ).stop();
                     self.actions.hurt.setEffectiveWeight( 1 ).stop();
                     self.actions.run.setEffectiveWeight( 1 ).stop();
                     self.actions.walk.setEffectiveWeight( 1 ).stop();
 
-                    // self.actions.attack.setLoop( THREE.LoopOnce, 0 );
-                    // self.actions.attack.clampWhenFinished = true;
-                    // self.actions.die.setLoop( THREE.LoopOnce, 0 );
-                    // self.actions.die.clampWhenFinished = true;
-                    // self.actions.hurt.setLoop( THREE.LoopOnce, 0 );
-                    // self.actions.hurt.clampWhenFinished = true;
+                    self.actions.attack.setLoop( THREE.LoopOnce, 0 );
+                    self.actions.attack.clampWhenFinished = true;
+                    self.actions.die.setLoop( THREE.LoopOnce, 0 );
+                    self.actions.die.clampWhenFinished = true;
+                    self.actions.hurt.setLoop( THREE.LoopOnce, 0 );
+                    self.actions.hurt.clampWhenFinished = true;
 
                 });
 
@@ -95,8 +88,6 @@ define(["THREE",
             },
 
             update: function (deltaTime, actualTime) {
-                // GameObjectBase.prototype.update.call(this, deltaTime, actualTime);
-
                 if (!this._innerObject3D) {
                     return;
                 }
@@ -138,17 +129,19 @@ define(["THREE",
             _getPhysicsBody: function () {
                 if (null === this.physicsBody) {
                     var self = this;
-                    var shape = new CANNON.Sphere(1);
+                    var bottomShape = new CANNON.Sphere(0.5);
+                    var topShape = new CANNON.Sphere(0.35);
                     var hfBody = new CANNON.Body({
-                        mass: 81
+                        mass: 81,
                         // type: CANNON.DYNAMIC,
-                        // angularDamping: 1.0,
-                        // linearDamping: 0.9
+                        angularDamping: 1.0,
+                        linearDamping: 0.9
                     });
-                    hfBody.addShape(shape);
+                    hfBody.addShape(bottomShape, 0).addShape(topShape, new CANNON.Vec3(0, 0, 0));
+                    hfBody.addShape(topShape, 0).addShape(topShape, new CANNON.Vec3(0, 0.75, 0));
 
                     this.physicsBody = hfBody;
-                    this.physicsBodyPositionOffset.set(0, 1, 0);
+                    this.physicsBodyPositionOffset.set(0, 0.5, 0);
                     this.physicsBodyEulerOffset.set(0, 0, 0);
 
                     this.physicsBody.addEventListener("collide", $.proxy(this._collide_event, this));
